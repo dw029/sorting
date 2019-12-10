60bc49182ed453cabecb739de40af1d136eab5e5
dw029 <56631871+dw029@users.noreply.github.com>
2019-12-10 12:22:31 -0500

    Implemented comparator support

--- bubblesort.js
+++ bubblesort.js
@@ -1,4 +1,4 @@
-const bubbleSort = arr => {
+const bubbleSort = (arr, comparator = defaultComparator) => {
   if (!arr.length) {
     return arr
   }
@@ -7,7 +7,7 @@ const bubbleSort = arr => {
   while (sorted === false) {
     sorted = true
     for (let i = 0; i < arr.length; i++) {
-      if (arr[i] > arr[i + 1] && arr[i + 1]) {
+      if (comparator(arr[i], arr[i+1])) {
         sorted = false
         let temp = arr[i + 1]
         arr[i + 1] = arr[i]
@@ -17,6 +17,11 @@ const bubbleSort = arr => {
   }
 
   return arr
+}
+
+const defaultComparator = (a, b) => {
+  if (a > b) return true
+  if (a < b) return false
 }
 
 console.log(bubbleSort([4, 3, 17, 2, 9, 1]))
